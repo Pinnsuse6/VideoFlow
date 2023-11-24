4e0b3d332753c2ec422addfa14d7904919575554
KH L <1006187730@qq.com>
2023-11-24 21:01:12 +0800

    inference

--- inference.py
+++ inference.py
@@ -87,7 +87,7 @@ def count_parameters(model):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--mode', default='MOF')
-    parser.add_argument('--seq_dir', default='demo_input_images')
+    parser.add_argument('--seq_dir', default='kaggle/input/videoflow-code/demo_input_images')
     parser.add_argument('--vis_dir', default='kaggle/working/demo_flow_vis/')
     
     args = parser.parse_args()
