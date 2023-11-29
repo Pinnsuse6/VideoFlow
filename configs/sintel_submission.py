28f43cf8dcdc5a224f100ed159fca632278e2854
KH L <1006187730@qq.com>
2023-11-29 10:15:10 +0800

    sintel_submission.py

--- configs/sintel_submission.py
+++ configs/sintel_submission.py
@@ -17,7 +17,7 @@ _CN.network = 'BOFNet'
 
 _CN.restore_ckpt = False
 
-_CN.model = "VideoFlow_ckpt/BOF_sintel_train.pth"
+_CN.model = "/kaggle/input/videoflow-code/VideoFlow_ckpt/BOF_sintel_train.pth"
 
 
 _CN.BOFNet = CN()
